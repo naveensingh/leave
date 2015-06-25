c03b49e98a9836e764b9dd723a064179bdf061d7
Naveen Singh <naveensingh.org@gmail.com>
2015-06-25 20:50:51 +0530

    initailized the project with custom settings

---
+++ settings/common.py
@@ -0,0 +1,88 @@
+import os
+
+
+# =============================================================================
+# Which WEB_ENV?
+# =============================================================================
+# Valid options - local, dev, qa, prod
+WEB_ENV = 'local'
+
+# Branch and version number
+BRANCH = 'dev'
+VERSION = 'v0.0.0'
+# =============================================================================
+# PATHS
+# =============================================================================
+# Full filesystem path to the project.
+# based on common.py is in <PROJECT_ROOT>/settings
+PROJECT_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                            os.pardir)
+
+# Name of the directory for the project.
+PROJECT_DIRNAME = PROJECT_ROOT.split(os.sep)[-1]
+
+# Package/module name to import the root urlpatterns from for the project.
+# ROOT_URLCONF = "%s.urls" % PROJECT_DIRNAME
+ROOT_URLCONF = "settings.urls"
+
+# Put strings here, like "/home/html/django_templates"
+# or "C:/www/django/templates".
+# Always use forward slashes, even on Windows.
+# Don't forget to use absolute paths, not relative paths.
+TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, "templates"),)
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+ALLOWED_HOSTS = ["127.0.0.1"]
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+SECRET_KEY = '57)p_wybgbiof@xaph3r=&t29d4yydl)-i(4px4kc&wwlah&k+'
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+
