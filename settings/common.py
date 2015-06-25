55dafdac07c4c13f02065232f9e2112a5a606c41
Naveen Singh <naveensingh.org@gmail.com>
2015-06-25 20:57:37 +0530

    added admin static files

--- settings/common.py
+++ settings/common.py
@@ -67,22 +67,11 @@ DATABASES = {
     }
 }
 SECRET_KEY = '57)p_wybgbiof@xaph3r=&t29d4yydl)-i(4px4kc&wwlah&k+'
-# Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
 STATIC_URL = '/static/'
-
+STATIC_ROOT = PROJECT_ROOT + "/static"
