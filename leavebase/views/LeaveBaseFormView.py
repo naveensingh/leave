9b7f0762b9c3f0160f95edbc0ab7f563f4c7c223
Naveen Singh <naveensingh.org@gmail.com>
2015-07-09 10:49:30 +0530

    added improved leave form with validations and new table design for list of leaves

--- leavebase/views/LeaveBaseFormView.py
+++ leavebase/views/LeaveBaseFormView.py
@@ -1,7 +1,11 @@
+from datetime import date
+
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import CreateView
-from django.contrib.messages import error
+from django.contrib.messages import error, info
+from django.utils.translation import ugettext_lazy as _
+
 from leavebase.models import LeaveBase
 
 
@@ -37,10 +41,36 @@ class ApplyForLeaveView(CreateView):
             reason = data.get("reason")[0]
             starting_from = data.get("starting_from")[0]
             ending_on = data.get("ending_on")[0]
-            leave = LeaveBase(user_id=self.request.user.id, name=self.request.user.first_name, reason=reason,
-                              starting_from=starting_from,
-                              ending_on=ending_on)
-            leave.save()
-            return HttpResponseRedirect("/leave/all")
+
+            try:
+                no_of_days = self.get_no_of_days(starting_from, ending_on).days
+            except:
+                no_of_days = 0
+            try:
+                leave = LeaveBase(user_id=self.request.user.id, name=self.request.user.first_name, reason=reason,
+                                  starting_from=starting_from,
+                                  ending_on=ending_on, no_of_days=no_of_days)
+                leave.save()
+                info(self.request, _("Applied"))
+                return HttpResponseRedirect("/leave/all")
+            except:
+                error(self.request, _("Failed to apply"))
+
         context = self.get_context()
         return render(request, self.template_name, context)
+
+    def get_no_of_days(self, starting_from, ending_on):
+        get_starting_date = self.make_date_list(str(starting_from))
+        get_ending_date = self.make_date_list(str(ending_on))
+
+        get_days = get_ending_date - get_starting_date
+
+        return get_days
+
+    def make_date_list(self, get_date):
+        replace_starting_from = str(get_date).replace("/", ",")
+        get_starting_date_list = replace_starting_from.rstrip(",").split(',')
+        get_starting_date = date(int(get_starting_date_list[2]), int(get_starting_date_list[1]),
+                                 int(get_starting_date_list[0]))
+
+        return get_starting_date
